de6b3d5d396e3da08759b29ef323edabdbfb5a4b
Animesh Singh <animeshsingh.mail@gmail.com>
2023-04-20 06:41:05 +0530

    New tracked files push

--- client/src/App.js
+++ client/src/App.js
@@ -1,62 +1,22 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
+import AddUserForm from './components/AddUserForm';
+import UpdateUserForm from './components/UpdateUserForm';
+import DeleteUser from './components/DeleteUserForm';
+import AddHotel from './components/AddHotelForm';
+import UpdateHotel from './components/UpdateHotelForm';
+import DeleteHotel from './components/DeleteHotelForm';
 
-const AddUserForm = () => {
-  const [name, setName] = useState('');
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [license, setLicense] = useState('');
-  const [licenseNumber, setLicenseNumber] = useState('');
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    axios.post('http://localhost:3000/api/user/add', {
-      name,
-      username,
-      email,
-      license,
-      license_number: licenseNumber,
-    })
-      .then(response => {
-        console.log(response.data);
-        // Reset the form fields
-        setName('');
-        setUsername('');
-        setEmail('');
-        setLicense('');
-        setLicenseNumber('');
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  };
-
+const App = () => {
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Name:</label>
-        <input type="text" value={name} onChange={event => setName(event.target.value)} />
-      </div>
-      <div>
-        <label>Username:</label>
-        <input type="text" value={username} onChange={event => setUsername(event.target.value)} />
-      </div>
-      <div>
-        <label>Email:</label>
-        <input type="email" value={email} onChange={event => setEmail(event.target.value)} />
-      </div>
-      <div>
-        <label>License:</label>
-        <input type="text" value={license} onChange={event => setLicense(event.target.value)} />
-      </div>
-      <div>
-        <label>License Number:</label>
-        <input type="text" value={licenseNumber} onChange={event => setLicenseNumber(event.target.value)} />
-      </div>
-      <button type="submit">Add User</button>
-    </form>
+    <div>
+      <h1>Add User</h1>
+      <AddHotel/>
+      <h1>Update User</h1>
+      <UpdateHotel/>
+      <h1>DeleteHotel</h1>
+      <DeleteHotel/>
+    </div>
   );
 };
 
-export default AddUserForm;
+export default App;
